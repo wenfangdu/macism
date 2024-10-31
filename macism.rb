f9483eea40cfe2517499c30f8a21a4742d7c7479
Mao <code@mao.to>
2024-10-31 22:50:08 +0800

    update sha256 in brew formula

--- macism.rb
+++ macism.rb
@@ -6,10 +6,10 @@ class Macism < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/laishulu/macism/releases/download/v#{version}/macism-arm64"
-      sha256 "PLACE_HOLDER"
+      sha256 "cbf462686551de911c3278d4d299a62c0e61230bd7b1897a4e7f1d49d951f9a1"
     else
       url "https://github.com/laishulu/macism/releases/download/v#{version}/macism-x86_64"
-      sha256 "PLACE_HOLDER"
+      sha256 "21ca3d27cb20472f72e99e66f87e9e771ec0a13663b4900aeccda40380aec834"
     end
   end
 
